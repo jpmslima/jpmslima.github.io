1433189ca96e66dcd6201aef4d7d936e740aac35
jpmslima <jpmslima@gmail.com>
2024-03-03 19:35:57 -0300

    Deployed f5db796 with MkDocs version: 1.5.3

---
+++ STR8figs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+wget 'https://drive.google.com/uc?export=download&id=1cR7PHWZ9OncU5Hayo_XMfCHbqHEGh16H' -O str8-fig1.png
+wget 'https://drive.google.com/uc?export=download&id=1BFwhmsaQCQjX2-JfF_-wBlNGHjcr2ElT' -O str8-fig2.png
+wget 'https://drive.google.com/uc?export=download&id=1lkYvluhaz5ZvK55UqD33pCTqnVPzR8Sx' -O str8-fig3.png
+wget 'https://drive.google.com/uc?export=download&id=1GBTU5jq0uauHLCnL-MHizW9l9rY2V687' -O str8-fig4.png
+wget 'https://drive.google.com/uc?export=download&id=1fL1kvDEQbfIts7lS56QrvY0uL79F6smg' -O str8-fig5.png
+wget 'https://drive.google.com/uc?export=download&id=1EIQsRlz4wL-VHMJzrHWbRXVfwJmouxew' -O str8-fig6.png
