76aaacd50f97bc9f91d0d808df7bb781699c2338
jpmslima <jpmslima@gmail.com>
2024-03-04 16:53:16 -0300

    Deployed 0fcf041 with MkDocs version: 1.5.3

---
+++ str20figs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+wget 'https://drive.google.com/uc?export=download&id=1w_OjLVxTqOrAcwe_cPA1tuGu13blmZLy' -O str20-fig1.png
+wget 'https://drive.google.com/uc?export=download&id=1XKSdanEl4OnXqxd7sjw_gv-2yJ-pFkRu' -O str20-fig2.png
+wget 'https://drive.google.com/uc?export=download&id=1UT-dKYEhhXBlP67-s7PFq3wpx_rl0zlc' -O str20-fig3.png
+wget 'https://drive.google.com/uc?export=download&id=1laU-L2jPHlrtvjWtzTQr9k_XPwtcxFxs' -O str20-fig4.png
+wget 'https://drive.google.com/uc?export=download&id=1eFnPSuQ9BvpwV7Af8WCCEIy5u7hphLJm' -O str20-fig5.png
+wget 'https://drive.google.com/uc?export=download&id=1Ea0-Onc9orIpf60y1pnc7dIElfH0jIGf' -O str20-fig6.png
+wget 'https://drive.google.com/uc?export=download&id=1wQefBZl3jOA4niqCKJdIgPylaN4w3Da0' -O str20-fig7.png
+wget 'https://drive.google.com/uc?export=download&id=1D6_5PEc8f_5t6CbJdtHKOcBjT6B8z2je' -O str20-fig8.png
