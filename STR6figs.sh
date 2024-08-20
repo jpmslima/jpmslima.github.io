9266beb9fa23dd3a09a867b2d25846127d9a8b45
jpmslima <jpmslima@gmail.com>
2024-08-20 09:34:55 -0300

    update082024

--- STR6figs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-wget 'https://drive.google.com/uc?export=download&id=10jRWeYu3EUTkyWeGmlJWx5PEuiqgXkbE' -O st6-fig1.png
-wget 'https://drive.google.com/uc?export=download&id=1xywU6wkBSHbC9TCFPLl47iadvIxRox8b' -O st6-fig2.png
-wget 'https://drive.google.com/uc?export=download&id=1pC9RL1ek60RGpuis9Xj3qQb4bFn2wMRA' -O st6-fig3.png
-wget 'https://drive.google.com/uc?export=download&id=1AoPTO3HiV4ceKIftc3__KcNTepkAszHA' -O st6-fig4.png
-wget 'https://drive.google.com/uc?export=download&id=1HOV1ubdDjy40N7mDCb3LV2YIMjw8VRGR' -O st6-fig5.png
-wget 'https://drive.google.com/uc?export=download&id=1pRbMfADGeMsWLWX21Cd2mVwfA2Xl84PX' -O st6-fig6.png
--- str23figs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-wget 'http://www.mdtutorials.com/gmx/lysozyme/Images/system_solv_ions.jpg'
-wget 'http://www.mdtutorials.com/gmx/lysozyme/Images/plot_lyso_em_potential.png'
-wget 'http://www.mdtutorials.com/gmx/lysozyme/Images/plot_lyso_nvt_temperature.png'
-wget 'http://www.mdtutorials.com/gmx/lysozyme/Images/plot_lyso_npt_pressure.png'
-wget 'http://www.mdtutorials.com/gmx/lysozyme/Images/plot_lyso_npt_density.png'
-wget 'http://www.mdtutorials.com/gmx/lysozyme/Images/plot_lyso_md_rmsd.png'
-wget 'http://www.mdtutorials.com/gmx/lysozyme/Images/plot_lyso_md_rg.png'
