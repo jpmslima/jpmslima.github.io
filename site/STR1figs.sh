4dccf0196fd8522ce709f3a35066ff1c56a8fc76
jpmslima <jpmslima@gmail.com>
2024-08-20 13:13:21 -0300

    082024

---
+++ site/STR1figs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+wget 'https://drive.google.com/uc?export=download&id=1MysaEIqb01M-8SCXYQDl-BH0xfAPV-yC' -O str1-fig1.png
+wget 'https://drive.google.com/uc?export=download&id=19X_94UliYjFnYAtBG6AJa39hKHNjWH6e' -O str1-fig2.png
+wget 'https://drive.google.com/uc?export=download&id=15_dMmoHIM8JwEAoy7kA9OnC53_40zCLl' -O str1-fig3.png
+wget 'https://drive.google.com/uc?export=download&id=1R7GlackI02XCJ8xP5S5Y_0PwDiZekVCl' -O str1-fig4.png
+wget 'https://drive.google.com/uc?export=download&id=1745jDouX6bg45-toWRClW0ogQrka0WNd' -O str1-fig5.png
+wget 'https://drive.google.com/uc?export=download&id=1KCVF8Xd4Z9OyLxadIl5wZOgm-MDtDjS2' -O str1-fig6.png
+wget 'https://drive.google.com/uc?export=download&id=1HKCx8JK90LBkcqJkrQHJnirA4Btl1bSm' -O str1-fig7.png
+wget 'https://drive.google.com/uc?export=download&id=13z9uCeWPBjQOyz1LkkbIMfJnwESF__5Y' -O str1-fig8.png
+wget 'https://drive.google.com/uc?export=download&id=1KruaYXmrZLyyubEZtKRrRHzp-ABfmqTt' -O str1-fig9.png
+wget 'https://drive.google.com/uc?export=download&id=1VQALZVFUiPpcUKwXgyy4yQhM78oAOUwY' -O str1-fig10.png
+wget 'https://drive.google.com/uc?export=download&id=1DIW1ouhWIYii2Hm-Usjrqb3_v3PEKDwV' -O str1-fig11.png
+wget 'https://drive.google.com/uc?export=download&id=1wU_0UF1FfQ-PjFsuAD_QR6Bxg9etRg5Q' -O str1-fig12.png
+wget 'https://drive.google.com/uc?export=download&id=1cgfkh8O7p61h4D3XTgKahdEgw_xl-1RY' -O str1-fig13.png
+wget 'https://drive.google.com/uc?export=download&id=1PR5Xb9Aqu_ClfsEk7IDVdi9CeP75L52-' -O str1-fig14.png
+wget 'https://drive.google.com/uc?export=download&id=1CtMw3yazqtNXCTcBMKMuNZxnkuSgjMvH' -O str1-fig15.png
---
+++ site/UC_figs_get.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+wget 'https://drive.google.com/uc?export=download&id=1Rgh88avopwh7YtUKsD5hXMwF3BFH5-Kj' -O UC-fig1.png
+wget 'https://drive.google.com/uc?export=download&id=1xeSo8MOvqmgO4q_6lyidauaKOjdTPu3t' -O UChile.png
+wget 'https://drive.google.com/uc?export=download&id=1CZ0GD_Ad0QpCj34uw0F6Vv_RZdh2eEa4' -O 1sT-fig1.png
+wget 'https://drive.google.com/uc?export=download&id=1au3i82_mPv438f9MPmhx80AmgYONDwSg' -O 1sT-fig2.png
+wget 'https://drive.google.com/uc?export=download&id=1z_pYNKMou0TSJxU91ODerO1qKsnl3xul' -O 1sT-fig3.png
+wget 'https://drive.google.com/uc?export=download&id=1fewCtlD82mHeDIoYGY4FZZRXPgJCcXn8' -O 1sT-fig4.png
+wget 'https://drive.google.com/uc?export=download&id=1IEaubZlhl6s0cQpJR1uAFxsGO4zw2zRi' -O 1sT-fig5.png
+wget 'https://drive.google.com/uc?export=download&id=1_0U_iLyKqx2TFMkfz5WwAMue8YBCfGWU' -O 1sT-fig6.png
+wget 'https://drive.google.com/uc?export=download&id=18Hlz0XOhP1s029UPhPW20LNrQiqAx4iI' -O 1sT-fig7.png
+wget 'https://drive.google.com/uc?export=download&id=1dBm8dDWdTwoUrV8uA3QZaUrGWXW2hhqT' -O 1sT-fig8.png
+wget 'https://drive.google.com/uc?export=download&id=1ZyRW24ZBFOpqPSs6yR8ePEjvCSAIaLHv' -O SS-fig1.png
+wget 'https://drive.google.com/uc?export=download&id=1jb3jjcpSWokWIjkw22n8dL8GyDaYm6M7' -O SS-fig2.png
+wget 'https://drive.google.com/uc?export=download&id=1hzllDsk1h0VgDmC5IGowRRpJDBgyivcZ' -O SS-fig3.png
+wget 'https://drive.google.com/uc?export=download&id=1F4KiIXZK87m73BTPh-EH74Sa9wI32V_f' -O SS-fig4.png
+wget 'https://drive.google.com/uc?export=download&id=1V2kvu67lZofJxsPWbvyah65K2e1LvyTC' -O SS-fig5.png
+wget 'https://drive.google.com/uc?export=download&id=1vSNBqsSBgohUY1xBCTsHZw6Qyxth9ZXM' -O SS-fig6.png
+wget 'https://drive.google.com/uc?export=download&id=1N1QJj3E_-CVaBCOxvb2-JHP49ZauZ3Jo' -O SS-fig7.png
+wget 'https://drive.google.com/uc?export=download&id=1D6e1pvkYsXm4oQZ_OiTCiIrz6LkmR29F' -O SS-fig8.png
+wget 'https://drive.google.com/uc?export=download&id=1Nrd3iDTdAqqaabgB4ZZJQU0si_FQ8U9O' -O SS-fig9.png
+wget 'https://drive.google.com/uc?export=download&id=16Jq02ybg0Jh5GtS51CHNAcq-WqLJATji' -O SS-fig10.png
+
+
+
+
+
+
+
